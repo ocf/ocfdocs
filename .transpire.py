e4e346177a4f6df3b4bcca16a347ad21ae9ab247
Joe Wang <69328902+Storce@users.noreply.github.com>
2025-02-17 12:46:08 -0800

    fix: build ingress from Service

--- .transpire.py
+++ .transpire.py
@@ -21,7 +21,7 @@ def objects():
         port_on_svc=80,
     )
     
-    ing = Ingress(
+    ing = Ingress.from_svc(
         svc=svc,
         # TODO: Define domain name
         host="//mkdocs.ocf.berkeley.edu",
